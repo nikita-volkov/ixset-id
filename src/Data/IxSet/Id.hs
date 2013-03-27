5b4c9ce6d29464fdf16825f309837d6386838973
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-03-27 22:44:14 +0400

    IdentifiedIxSet -> IIS

--- src/Data/IxSet/Id.hs
+++ src/Data/IxSet/Id.hs
@@ -25,82 +25,82 @@ instance (SafeCopy a) => SafeCopy (Identified a) where
 
 
 
--- | A wrapper over IxSet managing the identification features
-data IdentifiedIxSet a = 
-  IdentifiedIxSet {
-    identifiedIxSetNextId :: Id a,
-    identifiedIxSetValue :: IxSet (Identified a)
+-- | Identified IxSet. A wrapper over IxSet managing the identification features
+data IIS a = 
+  IIS {
+    iisNextId :: Id a,
+    iisValue :: IxSet (Identified a)
   }
   deriving (Eq, Ord, Show, Data, Typeable)
 
-instance (Indexable (Identified a), SafeCopy a, Typeable a, Ord a) => SafeCopy (IdentifiedIxSet a) where
-  putCopy IdentifiedIxSet{..} = contain $ do
-    safePut $ idValue identifiedIxSetNextId
-    safePut $ IxSet.toList identifiedIxSetValue
+instance (Indexable (Identified a), SafeCopy a, Typeable a, Ord a) => SafeCopy (IIS a) where
+  putCopy IIS{..} = contain $ do
+    safePut $ idValue iisNextId
+    safePut $ IxSet.toList iisValue
   getCopy = contain $ 
-    IdentifiedIxSet 
+    IIS 
       <$> (Id <$> safeGet)
       <*> (IxSet.fromList <$> safeGet)
 
 
 
-emptyIIS :: (Indexable (Identified a)) => IdentifiedIxSet a
-emptyIIS = IdentifiedIxSet (Id 1) IxSet.empty
+emptyIIS :: (Indexable (Identified a)) => IIS a
+emptyIIS = IIS (Id 1) IxSet.empty
 
 insertIIS :: (Ord a, Typeable a, Indexable (Identified a)) =>
   a -> 
-  IdentifiedIxSet a -> (Identified a, IdentifiedIxSet a)
+  IIS a -> (Identified a, IIS a)
 insertIIS v = runState $ do
-  IdentifiedIxSet{..} <- get
-  let identified = Identified identifiedIxSetNextId v
-  put $ IdentifiedIxSet
-    (succ identifiedIxSetNextId)
-    (IxSet.insert identified identifiedIxSetValue)
+  IIS{..} <- get
+  let identified = Identified iisNextId v
+  put $ IIS
+    (succ iisNextId)
+    (IxSet.insert identified iisValue)
   return identified
 
 -- | If a lookup succeeds, replace the match with a value, otherwise insert it.
 replaceIIS :: (Ord a, Typeable a, Indexable (Identified a), Typeable k) =>
   a -> 
   k ->
-  IdentifiedIxSet a -> (Identified a, IdentifiedIxSet a)
+  IIS a -> (Identified a, IIS a)
 replaceIIS v k = runState $ do
-  iis@IdentifiedIxSet{..} <- get
-  case getOne $ identifiedIxSetValue @= k of
+  iis@IIS{..} <- get
+  case getOne $ iisValue @= k of
     Just existing@(Identified id _) -> do
       let new = Identified id v
-      put $ IdentifiedIxSet identifiedIxSetNextId 
+      put $ IIS iisNextId 
         $ IxSet.insert new
         $ IxSet.delete existing
-        $ identifiedIxSetValue
+        $ iisValue
       return new
     Nothing -> do
-      let identified = Identified identifiedIxSetNextId v
-      put $ IdentifiedIxSet
-        (succ identifiedIxSetNextId)
-        (IxSet.insert identified identifiedIxSetValue)
+      let identified = Identified iisNextId v
+      put $ IIS
+        (succ iisNextId)
+        (IxSet.insert identified iisValue)
       return identified
 
 alterIIS :: (Ord a, Typeable a, Indexable (Identified a), Typeable k) =>
   (Maybe a -> Maybe a) -> 
   k -> 
-  IdentifiedIxSet a -> (Maybe (Identified a), IdentifiedIxSet a)
+  IIS a -> (Maybe (Identified a), IIS a)
 alterIIS f k = runState $ do
-  iis@IdentifiedIxSet{..} <- get
-  case getOne $ identifiedIxSetValue @= k of
+  iis@IIS{..} <- get
+  case getOne $ iisValue @= k of
     Just identified@(Identified id value) -> 
       case f $ Just value of
         Just newValue -> do
           let newIdentified = Identified id $ newValue
           put $ iis {
-            identifiedIxSetValue = 
+            iisValue = 
               IxSet.insert newIdentified $ IxSet.delete identified 
-                $ identifiedIxSetValue
+                $ iisValue
           }
           return $ Just newIdentified
         Nothing -> do
           put $ iis {
-            identifiedIxSetValue = 
-              IxSet.delete identified $ identifiedIxSetValue
+            iisValue = 
+              IxSet.delete identified $ iisValue
           }
           return Nothing
     Nothing -> 
@@ -110,16 +110,16 @@ alterIIS f k = runState $ do
 
 deleteIIS :: (Ord a, Typeable a, Indexable (Identified a)) =>
   Identified a ->
-  IdentifiedIxSet a -> IdentifiedIxSet a
+  IIS a -> IIS a
 deleteIIS v iis = 
   iis {
-    identifiedIxSetValue = IxSet.delete v $ identifiedIxSetValue iis
+    iisValue = IxSet.delete v $ iisValue iis
   }
 
 deleteAtIIS :: (Ord a, Typeable a, Indexable (Identified a), Typeable k) =>
   k ->
-  IdentifiedIxSet a -> IdentifiedIxSet a
+  IIS a -> IIS a
 deleteAtIIS k iis = 
   iis {
-    identifiedIxSetValue = IxSet.deleteIx k $ identifiedIxSetValue iis
+    iisValue = IxSet.deleteIx k $ iisValue iis
   }
